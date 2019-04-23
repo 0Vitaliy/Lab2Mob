fb58c3c965924042ec6a8cfd910940c21ef8c27d
Vitalic <0makovskyi@gmail.com>
2019-04-23 14:43:31 +0300

    fixBug

--- app/src/main/java/com/example/makovskyi41pilab02/MainTwoActivity.java
+++ app/src/main/java/com/example/makovskyi41pilab02/MainTwoActivity.java
@@ -17,7 +17,7 @@ public class MainTwoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        textView.findViewById(R.id.textView);
+        textView=findViewById(R.id.textView);
 //
 
         Intent intent =getIntent();
